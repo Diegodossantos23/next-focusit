b98209ca18bf9505b9b1a7b298beb0eb53c004f9
Diegodossantos23 <diegodossantos900@outlook.com>
2021-02-23 14:45:24 -0300

    criei os estilos globais  e a reponsividade

--- focusit/src/App.tsx
+++ focusit/src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import {Button} from './components/Button'
+import './styles/global.css'
 
 const App = () => {
   return (
     <div>
      <h1>Hello Focusit</h1>
      
-     <Button
-      color="blue">
-       ButtonOne
-     </Button>
-
-     <Button
-      color="purple">
-       ButtonTwo
-     </Button>
-
-     <Button
-      color="grey">
-       ButtonThree
-     </Button>
+     
     </div>
   );
 }
