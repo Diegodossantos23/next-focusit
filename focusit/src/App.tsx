bd9a1f621312b986af95e8028c1193a412d16877
Diegodossantos23 <diegodossantos900@outlook.com>
2021-02-23 14:11:57 -0300

    passando props

--- focusit/src/App.tsx
+++ focusit/src/App.tsx
@@ -5,9 +5,21 @@ const App = () => {
   return (
     <div>
      <h1>Hello Focusit</h1>
-     <Button color="blue"/>
-     <Button color="purple"/>
-     <Button color="grey"/>
+     
+     <Button
+      color="blue">
+       ButtonOne
+     </Button>
+
+     <Button
+      color="purple">
+       ButtonTwo
+     </Button>
+
+     <Button
+      color="grey">
+       ButtonThree
+     </Button>
     </div>
   );
 }
--- focusit/src/components/Button.tsx
+++ focusit/src/components/Button.tsx
@@ -1,11 +1,16 @@
 interface ButtonProps {
     color: string;
+    children: string;
 }
 
 export const Button = (props: ButtonProps) => {
     return (
-        <button type="button" >
-            Testing
+        <button 
+            type="button"
+            style={{
+                backgroundColor: props.color
+            }} >
+                {props.children} <strong>0</strong>
         </button>
     )
 }
