996ecbb1c38dc04bc324627cecfad72b02652344
Diegodossantos23 <diegodossantos900@outlook.com>
2021-02-23 13:48:45 -0300

    Criei o componente Button e o importei no app.js

--- focusit/src/App.tsx
+++ focusit/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import {Button} from './components/Button'
 
 const App = () => {
   return (
     <div>
      <h1>Hello Focusit</h1>
+     <Button/>
     </div>
   );
 }
