725bef92d66dbb967d597b9705c3e9295390d457
Diegodossantos23 <diegodossantos900@outlook.com>
2021-02-25 16:18:01 -0300

    criei a funcionalidade pra inicar o countdown

--- focusit-next/src/components/CountDown.tsx
+++ focusit-next/src/components/CountDown.tsx
@@ -1,17 +1,51 @@
+import { useEffect, useState } from 'react'
 import styles from '../styles/components/CountDown.module.css'
 
 export const CountDown = () => {
+    const [time, setTime] = useState(25 * 60)
+    const [active, setActive] = useState(false)
+    
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+    const [SecondLeft, SecondRight] = String(seconds).padStart(2, '0').split('');
+
+
+    const startCountDown = () => {
+        setActive(true)
+    }
+
+    useEffect(() => {
+        if(active && time > 0) {
+            setTimeout(() => {
+                setTime(time - 1)
+            }, 1000)
+        }
+    }, [active, time])
+
     return(
-        <div className={styles.countDownContainer}>
-            <div>
-                <span>2</span>
-                <span>5</span>
+        <div>
+            <div className={styles.countDownContainer}>
+                <div>
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>{SecondLeft}</span>
+                    <span>{SecondRight}</span>
+                </div>
             </div>
-            <span>:</span>
-            <div>
-                <span>0</span>
-                <span>0</span>
-            </div>
+
+            <button 
+                type="button" 
+                className={styles.countDownButton}
+                onClick={() => startCountDown()}
+                >
+
+                Start a cycle
+            </button>
         </div>
     )
 }
--- focusit-next/src/pages/index.tsx
+++ focusit-next/src/pages/index.tsx
@@ -1,14 +1,21 @@
 import {ExperienceBar} from '../components/ExperiencieBar'
 import { Profile } from '../components/Profile'
 import { CompletedChallenges } from '../components/CompletedChallenges'
+import { CountDown } from '../components/CountDown'
 
+import Head from 'next/head'
 
 import styles from '../styles/pages/Home.module.css'
-import { CountDown } from '../components/CountDown'
+
+
 
 const Home = () => {
   return (
     <div className={styles.container}>
+
+      <Head>
+          <title>Start | focus.it</title>
+      </Head>
 
      <ExperienceBar />
       <section>
