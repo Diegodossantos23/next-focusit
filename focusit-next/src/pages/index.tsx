9fdf4583d8d64625e579724b774460c4482ddd98
Diegodossantos23 <diegodossantos900@outlook.com>
2021-02-25 10:42:34 -0300

    Terminei de estilizar o completed challenges

--- focusit-next/src/pages/index.tsx
+++ focusit-next/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import {ExperienceBar} from '../components/ExperiencieBar'
 import { Profile } from '../components/Profile'
+import { CompletedChallenges } from '../components/CompletedChallenges'
 
 
 import styles from '../styles/pages/Home.module.css'
@@ -7,11 +8,12 @@ import styles from '../styles/pages/Home.module.css'
 const Home = () => {
   return (
     <div className={styles.container}>
-      
+
      <ExperienceBar />
       <section>
         <div>
           <Profile/>
+          <CompletedChallenges/>
         </div>
         <div>
 
