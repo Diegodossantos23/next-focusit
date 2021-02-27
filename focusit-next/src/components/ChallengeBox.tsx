e2853112eeb2e6acc1e70a2c33c261208588ad4b
Diegodossantos23 <diegodossantos900@outlook.com>
2021-02-27 12:43:17 -0300

    criando o componente challengebox

---
+++ focusit-next/src/components/ChallengeBox.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/components/ChallengeBox.module.css'
+
+export const ChallengeBox = () => {
+    return(
+        <div className={styles.challengeBoxContainer}>
+
+        </div>
+    )
+}
